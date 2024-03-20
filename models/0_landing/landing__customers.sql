2f242293ee3ef2143da7b539f8ae95fe6769440e
esteban <estebanprietodocs@gmail.com>
2024-03-20 13:06:25 +0100

    new changes

--- models/0_landing/landing__customers.sql
+++ models/0_landing/landing__customers.sql
@@ -1,6 +1,6 @@
 
     select 
     
-    * exclude (_AIRBYTE_EXTRACTED_AT,_AIRBYTE_META,_AIRBYTE_RAW_ID)
+    * exclude (_AIRBYTE_EXTRACTED_AT,_AIRBYTE_META)
     
     from {{source('test2','sources__customers')}}
